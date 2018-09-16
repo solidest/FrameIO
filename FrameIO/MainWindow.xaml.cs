fd4d0865173e5a33ca83421afed35bf14558017e
solidest <solidest@163.com>
2018-09-16 11:24:44 +0800

    add main form

--- FrameIO/MainWindow.xaml.cs
+++ FrameIO/MainWindow.xaml.cs
@@ -24,5 +24,60 @@ namespace FrameIO
         {
             InitializeComponent();
         }
+
+        //窗体加载 UI初始化
+        private void MainFormLoaded(object sender, RoutedEventArgs e)
+        {
+            var toolBarThumb = MainToolBar.Template.FindName("ToolBarThumb", MainToolBar) as FrameworkElement;
+            if (toolBarThumb != null)
+            {
+                toolBarThumb.Visibility = Visibility.Collapsed;
+            }
+
+            var mainPanelBorder = MainToolBar.Template.FindName("MainPanelBorder", MainToolBar) as FrameworkElement;
+            if (mainPanelBorder != null)
+            {
+                mainPanelBorder.Margin = new Thickness(0);
+            }
+
+        }
+
+        //切换视图
+        private void SwitchView(object sender, RoutedEventArgs e)
+        {
+            if(edCode.Visibility == Visibility.Collapsed)
+            {
+                tbDocTree.Visibility = Visibility.Collapsed;
+                VSplitter.Visibility = Visibility.Collapsed;
+                tbPages.Visibility = Visibility.Collapsed;
+                edCode.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                edCode.Visibility = Visibility.Collapsed;
+                tbDocTree.Visibility = Visibility.Visible;
+                VSplitter.Visibility = Visibility.Visible;
+                tbPages.Visibility = Visibility.Visible;
+            }
+        }
+
+        //显示隐藏输出面板
+        private void OutDispHide(object sender, RoutedEventArgs e)
+        {
+            switch (this.HSplitter.Visibility)
+            {
+                case Visibility.Visible:
+                    HSplitter.Visibility = Visibility.Collapsed;
+                    gridMain.RowDefinitions[3].Height = new GridLength(0, GridUnitType.Pixel);
+                    break;
+                case Visibility.Collapsed:
+                    HSplitter.Visibility = Visibility.Visible;
+                    gridMain.RowDefinitions[3].Height = new GridLength(300, GridUnitType.Pixel);
+                    break;
+            }
+            e.Handled = true;
+        }
     }
+
+
 }
