b0976e4848564634f34b40fd7d4330e7b5586c41
solidest <solidest@163.com>
2018-12-26 09:17:29 +0800

    add frameioruntime2

---
+++ FrameIORuntime2/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameIORuntime2
+{
+    public class Class1
+    {
+    }
+}
