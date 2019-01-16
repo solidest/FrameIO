539e349aa35dc89d23c3da72e83ada0890b1d47f
solidest <solidest@163.com>
2019-01-16 09:22:16 +0800

    ready c++ version

--- FrameIORunX/FrameIORunX.cpp
+++ FrameIORunX/FrameIORunX.cpp
@@ -2,19 +2,20 @@
 
 #include "FrameIORunX.h"
 
+
 using namespace System::Runtime::InteropServices;
 using namespace System;
 
-extern "C" __declspec(dllexport) int NewFrameObject(char* frameName)
-{
-	String^ fraeName = Marshal::PtrToStringAnsi(static_cast<System::IntPtr>(frameName));
 
-	String^ A_path = "CSharp ascall string";
-	char* Ipath = (char*)(void*)Marshal::StringToHGlobalAnsi(A_path);
-	return Ipath;
+
+
+extern "C" __declspec(dllexport) void* CreateFioChannel(int channelType)
+{
+
 }
 
-extern "C" __declspec(dllexport) void release(void* b)
+
+extern "C" __declspec(dllexport) void ReleaseFioChannel(void* channelHandle)
 {
-	Marshal::FreeHGlobal(static_cast<System::IntPtr>(b));
+
 }
