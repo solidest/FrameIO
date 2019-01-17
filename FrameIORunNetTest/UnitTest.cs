e967186e227505f32944076f75d6f2895f923da4
solidest <solidest@163.com>
2019-01-17 11:51:40 +0800

    cpp is ok

--- FrameIORunNetTest/UnitTest.cs
+++ FrameIORunNetTest/UnitTest.cs
@@ -286,5 +286,55 @@ namespace FrameIORunNetTest
 
         #endregion
 
+        #region --动态数组--
+        [TestMethod]
+        public void A__FrameDynamicArray()
+        {
+            var tester1 = new frame_test_dynamicarray.sub_SingleByteArray();
+            var tester2 = new frame_test_dynamicarray.sub_SingleByteArray();
+
+            tester1.InitialParameter();
+            tester2.InitialParameter();
+
+            tester1.InitialChannelCHS(null);
+            tester2.InitialChannelCHC(null);
+
+            Assert.IsTrue(tester1.CHS.Open());
+            Assert.IsTrue(tester2.CHC.Open());
+            Assert.IsTrue(tester1.end.Count == 0);
+
+            tester1.end.Add(new frame_test_dynamicarray.Parameter<byte?>(10));
+            tester1.end.Add(new frame_test_dynamicarray.Parameter<byte?>(98));
+            tester1.end.Add(new frame_test_dynamicarray.Parameter<byte?>(2));
+
+            tester1.head.Value = (byte)tester1.end.Count;
+
+            tester1.len[0].Value = 2;
+            tester1.len[1].Value = 3;
+           
+
+
+            tester1.A_Send();
+
+            tester2.end.Add(new frame_test_dynamicarray.Parameter<byte?>(0));
+            tester2.end.Add(new frame_test_dynamicarray.Parameter<byte?>(0));
+            tester2.end.Add(new frame_test_dynamicarray.Parameter<byte?>(0));
+
+            tester2.A_Recv();
+
+
+            Assert.IsTrue(tester2.head.Value == 3);
+            Assert.IsTrue(tester2.len[0].Value == 2);
+            Assert.IsTrue(tester2.len[1].Value == 3);
+            Assert.IsTrue(tester2.end[0].Value == 10);
+            Assert.IsTrue(tester2.end[1].Value == 98);
+            Assert.IsTrue(tester2.end[2].Value == 2);
+        }
+
+
+        #endregion
+
+
     }
+
 }
